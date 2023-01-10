a1032f21f5b7506aa3259c39a1fa6a4a9e3e5b86
6pradeepthi <122279279+6pradeepthi@users.noreply.github.com>
2023-01-10 21:34:52 +0530

    Add files via upload

---
+++ day2 3program.py
@@ -0,0 +1,7 @@
+countOfWords = len("Hello This is python Programming".split())
+print("Count of words in the given sentence:", countOfWords)
+
+
+print(len("Hello This is python Programming".split()))
+
+print(len(input("enter input:").split()))
